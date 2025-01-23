d62e443fa2504ae27270ed3fb5f2778254c02f62
spoorthi0523 <2311cs010239@mallareddyuniversity.ac.in>
2025-01-23 11:36:09 +0530

    Reverse Integer

---
+++ Leetcode/LTCode002.java
@@ -0,0 +1,30 @@
+package Leetcode;
+
+public class LTCode002 {
+
+    public int reverse(int x) {
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+
+            reversed = reversed * 10 + digit;
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        LTCode002 solution = new LTCode002();
+        int result = solution.reverse(123);
+        System.out.println("Reversed: " + result);
+    }
+}
