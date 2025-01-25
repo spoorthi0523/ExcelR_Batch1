090f51e98a9990f8410bcdcf82e4e0c0f7480a8d
spoorthi0523 <2311cs010239@mallareddyuniversity.ac.in>
2025-01-25 11:17:21 +0530

    Convert uppercase char to lowercase

---
+++ Demo043.java
@@ -0,0 +1,18 @@
+package PS003;
+
+import java.util.Scanner;
+
+public class Demo043 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter an uppercase character:");
+        char upperCaseChar = scanner.next().charAt(0);
+
+        char lowerCaseChar = Character.toLowerCase(upperCaseChar);
+
+        System.out.println("The lowercase character is: " + lowerCaseChar);
+
+        scanner.close();
+    }
+}
