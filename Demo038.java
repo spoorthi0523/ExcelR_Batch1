5ccfbd50d9aa7c5b75230b921585c7fe0cd44e1f
spoorthi0523 <2311cs010239@mallareddyuniversity.ac.in>
2025-01-25 11:10:27 +0530

    Lowercase and Uppercase

---
+++ Demo038.java
@@ -0,0 +1,21 @@
+package PS003;
+
+import java.util.Scanner;
+
+//lowercase and uppercase
+public class Demo038 {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter a character: ");
+            char ch = scanner.next().charAt(0);
+
+            if (Character.isUpperCase(ch)) {
+                System.out.println(ch + " is an uppercase letter.");
+            } else if (Character.isLowerCase(ch)) {
+                System.out.println(ch + " is a lowercase letter.");
+            } else {
+                System.out.println(ch + " is not an alphabetic character.");
+            }
+        }
+    }
+}
