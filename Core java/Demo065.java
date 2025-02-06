7c7a8e16647feac12b7d466d27cf843839fa38a9
spoorthi0523 <2311cs010239@mallareddyuniversity.ac.in>
2025-02-06 10:56:24 +0530

    To print array elements.

---
+++ Core java/Demo065.java
@@ -0,0 +1,11 @@
+package PS003;
+
+import java.util.Arrays;
+
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 5, 7 };
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
