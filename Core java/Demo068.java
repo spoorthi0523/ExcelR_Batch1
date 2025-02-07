1bceada3887a43464e8d8ec5ec475a9c750ba5f0
spoorthi0523 <2311cs010239@mallareddyuniversity.ac.in>
2025-02-07 10:49:49 +0530

    To print count even numbers of an array

---
+++ Core java/Demo068.java
@@ -0,0 +1,16 @@
+package PS003;
+
+public class Demo068 {
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5, 6 };
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Count of even numbers: " + count);
+    }
+}
